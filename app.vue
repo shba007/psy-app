46baf871bc56e26ce1f72fbb74f253fa35fae740
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-08-16 17:28:32 +0530

    formatting fixed

--- app.vue
+++ app.vue
@@ -86,15 +86,15 @@ setInterval(ping, 10 * 60 * 1000)
 }
 
 *::-webkit-scrollbar {
-  @apply block w-[6px] bg-light-500 dark: bg-dark-400;
+  @apply block w-[6px] bg-light-500 dark:bg-dark-400;
 }
 
 *::-webkit-scrollbar-thumb {
-  @apply rounded-md bg-light-400 dark: bg-dark-600;
+  @apply rounded-md bg-light-400 dark:bg-dark-600;
 }
 
 body {
-  @apply text-black dark: text-white font-body bg-white dark:bg-black;
+  @apply text-black dark:text-white font-body bg-white dark:bg-black;
 }
 
 .nuxt-icon>svg {
